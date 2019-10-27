cfba385e2e3e9891744466cd096725abcb7ec4f7
Nguyen Hung Lam <nguyenhunglam.it@gmail.com>
2019-10-27 13:46:15 +0700

    First commit

---
+++ src/views/UserListView.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Text,
+  View
+} from "react-native";
+import getUserList from "../utils/getUserList"
+
+class UserListView extends React.Component {
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
+
+export default UserListView;
