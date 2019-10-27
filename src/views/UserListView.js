6525b3e2015799aaeb01d49ccd1313ab71a505cc
Nguyen Hung Lam <nguyenhunglam.it@gmail.com>
2019-10-27 14:03:10 +0700

    Display profile img and username from dummy

--- src/views/UserListView.js
+++ src/views/UserListView.js
@@ -1,14 +1,30 @@
 import React from "react";
 import {
   Text,
-  View
+  View,
+  FlatList,
+  Image
 } from "react-native";
-import getUserList from "../utils/getUserList"
+import getUserList from "../utils/getUserList";
 
 class UserListView extends React.Component {
   render() {
+    const userList = getUserList();
     return (
       <View>
+        <FlatList
+          data={userList.items}
+          keyExtractor={item => item.account_id}
+          renderItem={({ item }) => (
+            <View>
+              <Image
+                style={{ width: 50, height: 50 }}
+                source={{ uri: item.profile_image }}
+              />
+              <Text>{item.display_name}</Text>
+            </View>
+          )}
+        />
       </View>
     )
   }
